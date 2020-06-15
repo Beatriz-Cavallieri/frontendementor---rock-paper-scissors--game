f2418391b8fd1b5d1f7eeb08d40a145ee4f000ea
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-06-14 22:37:01 -0300

    adicionar funcionalidades mais básicas do jogo

--- script/main.js
+++ script/main.js
@@ -1,7 +1,13 @@
 const buttons = document.querySelectorAll('.option-btn')
+const scoreNumber = document.getElementById('score')
+const win = document.getElementById('win')
+const lose = document.getElementById('lose')
+const draw = document.getElementById('draw')
+const playAgain = document.getElementById('play-again')
 // opção 1 = pedra, opção 2 = papel, opção 3 = tesoura
 let houseChoice
 let userChoice
+let score = 0
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -11,16 +17,39 @@ buttons.forEach((button) => {
         if (userChoice === houseChoice) {
             //empate
             console.log('empate')
+            draw.classList.remove('hidden')
+            playAgain.classList.remove('hidden')
         }
         else if ((userChoice - houseChoice) === 1 || (userChoice - houseChoice) === -2) {
             //usuário ganha
             console.log('você ganhou')
+            score++
+            scoreNumber.innerText = score
+            win.classList.remove('hidden')
+            playAgain.classList.remove('hidden')
         }
         else {
             //usuário perde
             console.log('você perdeu')
+            score--
+            scoreNumber.innerText = score
+            lose.classList.remove('hidden')
+            playAgain.classList.remove('hidden')
         }
-        
+        buttons.forEach((button) => {
+            button.classList.add('hidden')
+        })
+    })
+})
+
+//botão de jogar novamente
+playAgain.addEventListener('click', e => {
+    win.classList.add('hidden')
+    lose.classList.add('hidden')
+    draw.classList.add('hidden')
+    playAgain.classList.add('hidden')
+    buttons.forEach((button) => {
+        button.classList.remove('hidden')
     })
 })
 
