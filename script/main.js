ed89f770ff76488da7ae6ef4ffae9649ef691811
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-06-24 21:10:52 -0300

    add images to option buttons, show what the user and the computer played

--- script/main.js
+++ script/main.js
@@ -1,53 +1,91 @@
 const optionButtons = document.querySelectorAll('.option-btn')
+const renderUserPlayed = document.getElementById('user-played')
+const renderHousePlayed = document.getElementById('house-played')
+//
+let score = 0
 const scoreNumber = document.getElementById('score')
 //
 const playAgain = document.getElementById('play-again')
 const result = document.getElementById('result')
-// opção 1 = pedra, opção 2 = papel, opção 3 = tesoura
-let houseChoice
-let userChoice
-let score = 0
+//
+let options = ["rock", "paper", "scissors"]
+let resultMessages = ["Draw", "You win", "You lose"]
+//
+let houseChoice = ""
+let userChoice = ""
 
 optionButtons.forEach((button) => {
     button.addEventListener('click', () => play(button))
 })
 
 function play(button) {
-    houseChoice = housePlays()
-    userChoice = parseInt(button.getAttribute('id'))
-    console.log(userChoice)
-    if (userChoice === houseChoice) {
-        //empate
-        showResult("Draw")
-    }
-    else if ((userChoice - houseChoice) === 1 || (userChoice - houseChoice) === -2) {
-        //usuário ganha
-        score++
-        showResult("You win")
-    }
-    else {
-        //usuário perde
-        score--
-        showResult("You lose")
-    }
-    scoreNumber.innerText = score
+    userChoice = button.getAttribute('id')
+    console.log('user: ' + userChoice)
     //esconde as opções
-    optionButtons.forEach((button) => button.classList.add('hidden'))
+    optionButtons.forEach((option) => option.classList.add('hidden'))
+    houseChoice = housePlays()
+    console.log('house: ' + houseChoice)
+    showPlayed()
+    showResult(userChoice, houseChoice)
 }
 
 playAgain.addEventListener('click', e => {
     //esconde os resultados e mostra as opções pra jogar novamente
-    //[win, lose, draw, playAgain].forEach((item) => item.classList.add('hidden'))
-    [result, playAgain].forEach((item) => item.classList.add('hidden'))
-    optionButtons.forEach((button) => button.classList.remove('hidden'))
+    [result, playAgain, renderUserPlayed, renderHousePlayed].forEach((item) => item.classList.add('hidden'))
+    optionButtons.forEach(button => button.classList.remove('hidden'))
 })
 
-function housePlays() {
-    //retorna número aleatório de 1 a 3, representando cada opção
-    return (Math.floor(Math.random() * 3) + 1)
+function showPlayed() {
+    // add style
+    renderUserPlayed.classList.add(userChoice)
+    renderHousePlayed.classList.add(houseChoice)
+    // show
+    renderUserPlayed.classList.remove('hidden')
+    renderHousePlayed.classList.remove('hidden')
 }
 
-function showResult(text) {
-    [result, playAgain].forEach((item) => item.classList.remove('hidden'))
-    result.innerText = text
+function compare(user, house) {
+    if (user === house) {
+        return (resultMessages[0])
+    }
+    else {
+        switch (user) {
+            case "rock":
+                if (house === 'paper') {
+                    return resultMessages[2]
+                }
+            case "paper":
+                if (house === "scissors") {
+                    return resultMessages[2]
+                }
+            case "scissors":
+                if (house === "rock") {
+                    return resultMessages[2]
+                }
+            default:
+                {
+                    return resultMessages[1]
+                }
+        }
+    }
+}
+
+function housePlays() {
+    let random = Math.floor(Math.random() * 3)
+    return options[random]
+}
+
+function showResult(user, house) {
+    let resultText = compare(userChoice, houseChoice)
+    console.log(resultText)
+    if (resultText === resultMessages[1]) {
+        score++ //user wins
+    }
+    else if (resultText === resultMessages[2]) {
+        score-- //user loses
+    }
+    scoreNumber.innerText = score
+    result.innerText = resultText
+    result.classList.remove('hidden')
+    playAgain.classList.remove('hidden')
 }
